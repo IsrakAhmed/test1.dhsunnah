f61d0ab4822a925721c38e2f65637340dd8c9685
IsrakAhmed <israkahmed7@gmail.com>
2026-01-15 16:49:49 +0600

    colon position fixed

--- resources/views/admin/idcard/print_mpdf_single.blade.php
+++ resources/views/admin/idcard/print_mpdf_single.blade.php
@@ -11,33 +11,33 @@
         <table style="width: 46mm; margin: 0 auto; border-collapse: collapse; font-size: 8pt;">
             <tr>
                 <td style="width: 14mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: left;">Father</td>
-                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: center;">:</td>
-                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white;">{{ $student->father_name }}</td>
+                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -2mm;">:</td>
+                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -1mm;">{{ $student->father_name }}</td>
             </tr>
             <tr>
                 <td style="width: 14mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: left;">Class</td>
-                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: center;">:</td>
-                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white;">{{ $student->class }}{{ $student->section ? ' - ' . $student->section : '' }}</td>
+                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -2mm;">:</td>
+                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -1mm;">{{ $student->class }}{{ $student->section ? ' - ' . $student->section : '' }}</td>
             </tr>
             <tr>
                 <td style="width: 14mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: left;">ID No</td>
-                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: center;">:</td>
-                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white;">{{ $student->registration_no }}</td>
+                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -2mm;">:</td>
+                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -1mm;">{{ $student->registration_no }}</td>
             </tr>
             <tr>
                 <td style="width: 14mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: left;">Roll</td>
-                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: center;">:</td>
-                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white;">{{ $student->roll_no }}</td>
+                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -2mm;">:</td>
+                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -1mm;">{{ $student->roll_no }}</td>
             </tr>
             <tr>
                 <td style="width: 14mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: left;">Mobile</td>
-                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: center;">:</td>
-                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white;">{{ $student->mobile_no }}</td>
+                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -2mm;">:</td>
+                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -1mm;">{{ $student->mobile_no }}</td>
             </tr>
             <tr>
                 <td style="width: 14mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: left;">Blood</td>
-                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; text-align: center;">:</td>
-                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white;">{{ $student->blood_group }}</td>
+                <td style="width: 2mm; padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -2mm;">:</td>
+                <td style="padding-bottom: 0.5mm; text-shadow: 0 0 2px white; padding-left: -1mm;">{{ $student->blood_group }}</td>
             </tr>
         </table>
     </div>
